67832140c21de4dc3a9fd3d12463b5683484ecd0
Romario <morrisromo9966@gmail.com>
2022-01-20 09:30:15 -0500

    adding new project w/ data collected

--- app.js
+++ app.js
@@ -10,12 +10,85 @@ const inquirer = require('inquirer')
 //     console.log('Portfoio complete! Check out index.html to see the output!');
 // });
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name?'
-        }
-    ])
-    .then(answers => console.log(answers));
+const promptUser = () => {
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?'
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'What is your GitHub username?'
+            },
+            {
+                type: 'input',
+                name: 'about',
+                message: 'Provide some information about yourself:'
+            }
+        ]);
+};
+
+console.log(`
+=================
+Add a New Project
+=================
+`);
+
+const promptProject = portfolioData => {
+    //if theres no 'projects' arrary property,create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'project',
+                message: 'What is the name of your project?'
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Provide a desctiption of the project:(required)'
+            },
+            {
+                type: 'checkbox',
+                name: 'stack',
+                message: 'What did you build this project with? (Check all that apply)',
+                choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node'],
+            },
+            {
+                type: 'confirm',
+                name: 'feature',
+                message: 'Would you like to feature this project?',
+                default: false
+            },
+            {
+                type: 'confirm',
+                name: 'confirmAddProject',
+                message: 'Would you like to enter another project?',
+                default: false
+            }
+
+        ])
+        .then(projectData => {
+            portfolioData.projects.push(projectData);
+            // if user confirms add more objcet
+            if (projectData.confirmAddProject) {
+                return promptProject(portfolioData);
+            } else {
+                // if not return the users data in object 
+                return portfolioData;
+            }
+        });
+};
+
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    })
